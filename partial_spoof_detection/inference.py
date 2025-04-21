454a62c5a0a293117fef88955cd26893ae30d5ab
ductuantruong <ductuanpy99@gmail.com>
2025-04-21 14:58:31 +0800

    add files

---
+++ partial_spoof_detection/inference.py
@@ -0,0 +1,225 @@
+import torch,argparse
+import torch.nn.functional as F
+import numpy as np
+from libs.dataloader.data_io import get_dataloader
+from models.conformer import FrameConformer 
+from libs.tool import *
+import os
+from libs.startup_config import set_random_seed
+import warnings
+import soundfile as sf
+import torch.nn.functional as F
+import json
+
+warnings.filterwarnings("ignore")
+__author__ = "Junyan Wu"
+__email__ = "wujy298@mail2.sysu.edu.cn"
+############EVAL CFPRF################
+import soundfile as sf
+import torchaudio
+    
+def print_spoof_timestamps(pred, rso=20):
+    start = None
+    txt_output = []
+    for i, value in enumerate(pred):
+        if value == 0:
+            if start is None:
+                start = i * rso
+        else:
+            if start is not None:
+                end = i * rso
+                txt_output.append(f"{start}ms - {end}ms")
+                start = None
+
+    # Check if the list ends with 0
+    if start is not None:
+        end = len(pred) * rso
+        txt_output.append(f"{start}ms - {end}ms")
+    return txt_output
+
+
+def Inference_mod(file_path, conformer_model, score_type_ouput, consolidate_output, device, base_rso, output_rso):
+    print("++++++++++++++++++inference++++++++++++++++++")
+    conformer_model.eval()
+    text_list = []
+    filename = os.path.basename(file_path)
+
+    with torch.no_grad():
+        file_type = file_path.split('.')[-1]
+        if file_type != 'mp3':
+            wave, sr = sf.read(file_path)
+            wave = np.expand_dims(wave, axis=0)  # [1, T]
+            wave = torch.Tensor(wave)
+        else:
+            wave, sr = torchaudio.load(file_path)
+
+        if sr != 16000:
+            wave = torchaudio.transforms.Resample(orig_freq=sr, new_freq=16000)(wave)
+
+        wave = wave.to(device)
+        total_samples = wave.shape[1]
+        sample_rate = 16000
+        chunk_samples = sample_rate * 4  # 4s chunk
+        pred_all = []
+
+        for start in range(0, total_samples, chunk_samples):
+            end = min(start + chunk_samples, total_samples)
+            chunk = wave[:, start:end]
+
+            # Zero pad if less than 4s
+            if chunk.shape[1] < chunk_samples:
+                padding = chunk_samples - chunk.shape[1]
+                chunk = torch.nn.functional.pad(chunk, (0, padding))
+
+            _, seg_score, _ = conformer_model(chunk)
+
+            seg_score = torch.squeeze(seg_score)
+
+            if score_type_ouput:
+                pred = (seg_score[:, 1] > args.threshold).long().tolist()
+            else:
+                pred = F.softmax(seg_score, dim=-1)[:, 0]
+                pred = F.avg_pool1d(
+                    pred.unsqueeze(0).unsqueeze(0),
+                    kernel_size=output_rso // base_rso,
+                    stride=output_rso // base_rso,
+                    ceil_mode=True
+                )
+                pred = pred.squeeze().tolist()
+
+            pred_all.extend(pred)
+
+        audio_duration = round(total_samples / sample_rate, 2)
+        text_list.append("Duration of audio: {}s".format(audio_duration))
+        text_list.append("Percentage Spoof audio: {}%".format(round((1 - sum(pred_all) / len(pred_all)) * 100, 2)))
+
+        if sum(pred_all) != len(pred_all):
+            text_list.append("Spoof audio segments:")
+            seg_text = print_spoof_timestamps(pred_all)
+            text_list += seg_text
+
+        text_list.append("Frame-level (1 frame is {} ms) prediction output (1 means bonafide frame, 0 means spoof frame):".format(args.output_rso))
+        text_list.append(str(pred_all))
+
+        # Save to JSON
+        if consolidate_output:
+            if os.path.exists(consolidate_output):
+                with open(consolidate_output, 'r') as jf:
+                    try:
+                        output_dict = json.load(jf)
+                    except json.JSONDecodeError:
+                        output_dict = {}
+            else:
+                output_dict = {}
+
+            output_dict[filename] = pred_all
+            with open(consolidate_output, 'w') as aw:
+                json.dump(output_dict, aw, indent=4)
+
+    return text_list
+
+def Inference(file_path, conformer_model, score_type_ouput, consolidate_output, device, base_rso, output_rso):
+    print("++++++++++++++++++inference++++++++++++++++++")
+    conformer_model.eval()
+    text_list = []
+    filename = os.path.basename(file_path)
+    with torch.no_grad():
+        file_type = file_path.split('.')[-1]
+        if file_type != 'mp3':
+            wave, sr = sf.read(file_path)
+            batch_x = torch.Tensor(np.expand_dims(wave, axis=0))
+        else:
+            wave, sr = torchaudio.load(file_path)
+            batch_x = wave
+        
+        if sr != 16000:
+            batch_x = torchaudio.transforms.Resample(
+                orig_freq=sr, new_freq=16000)(batch_x)
+
+        batch_x = batch_x.to(device)
+        batch_size = batch_x.size(0)
+        # inference FDN
+        _, seg_score, _ = conformer_model(batch_x) 
+        seg_score = torch.squeeze(seg_score)
+        if score_type_ouput:
+            pred = (seg_score[:, 1] > args.threshold).long().tolist()
+        else:
+            pred = F.softmax(seg_score, dim=-1)[:, 0]
+            pred = F.avg_pool1d(pred.unsqueeze(0).unsqueeze(0), kernel_size=output_rso//base_rso, stride=output_rso//base_rso, ceil_mode=True)
+            pred = pred.squeeze().tolist()
+        text_list.append("Duration of audio: {}s".format(round(wave.shape[-1]/sr, 2)))
+        text_list.append("Percentage Spoof audio: {}%".format(round((1-sum(pred)/len(pred))*100, 2)))
+        if sum(pred) != len(pred):
+            text_list.append("Spoof audio segments:")
+            seg_text = print_spoof_timestamps(pred)
+            text_list += seg_text
+        text_list.append("Frame-level (1 frame is {} ms) prediction output (1 means bonafide frame, 0 means spoof frame):".format(args.output_rso))
+        text_list.append(str(pred))
+        
+        # saving to the JSON output
+        if consolidate_output:
+        # read existing json file
+            if os.path.exists(consolidate_output):
+                with open(consolidate_output, 'r') as jf:
+                    try:
+                        output_dict = json.load(jf)
+                    except json.JSONDecodeError:
+                        output_dict = {}
+        		
+        	# write a new json file
+            else:
+                output_dict={}
+            output_dict[filename] = pred
+            with open(consolidate_output, 'w') as aw:
+	            json.dump(output_dict, aw, indent=4)
+    return text_list
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('python evaluate.py')
+    parser.add_argument('--seed', type=int, default=1234)
+    parser.add_argument('--save_path', type=str, default="result/") # ['HAD','PS','LAVDF']
+    parser.add_argument('--dn', type=str, default="PS") # ['HAD','PS','LAVDF']
+    parser.add_argument('--data_path', type=str)
+    parser.add_argument('--ckpt_path', type=str)
+    parser.add_argument('--threshold', type=float, default=0.9)
+    parser.add_argument('--consolidate_output', type=str, default="./result/consolidate_output.json")
+    parser.add_argument('--score_type_ouput', action='store_true', default=False)
+    parser.add_argument('--seql', type=int, default=1070)
+    parser.add_argument('--output_rso', type=int, default=1000)
+    parser.add_argument('--base_rso', type=int, default=20)
+    parser.add_argument('--glayer', type=int, default=1) 
+    parser.add_argument('--eval', action='store_true', default=True)
+
+     
+    args = parser.parse_args()
+    set_random_seed(args.seed)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    """loading FDN model"""
+    conformer_model = FrameConformer(seq_len=args.seql, gmlp_layers=args.glayer).to(device)
+    """ 
+    ./checkpoints
+    ├── 1FDN_HAD.pth
+    ├── 1FDN_LAVDF.pth
+    ├── 1FDN_PS.pth
+    ├── 2PRN_HAD.pth
+    ├── 2PRN_LAVDF.pth
+    ├── 2PRN_PS.pth
+    """
+    conformer_checkpoint="%s"%(args.ckpt_path)
+    conformer_model.load_state_dict(torch.load(conformer_checkpoint, map_location=device))
+    """makedir"""
+    #dict_save_path=os.path.join(args.save_path,'dict/%s_'%(args.dn))
+    #csv_save_path=os.path.join(args.save_path,'pd/%s_'%(args.dn))
+    #os.makedirs(os.path.dirname(dict_save_path),exist_ok=True)
+    #os.makedirs(os.path.dirname(csv_save_path),exist_ok=True)
+
+    os.makedirs(args.save_path, exist_ok=True)
+    ###########INFERENCE#############
+    output_text = Inference_mod(args.data_path, conformer_model, args.score_type_ouput, args.consolidate_output, device, args.base_rso, args.output_rso)
+    txt_file_name= args.data_path.split('/')[-1].split('.')[0] + '.txt'
+    with open(os.path.join(args.save_path, txt_file_name), 'w+') as fh:
+        fh.write('\n'.join(output_text) + '\n')
+    print("Output file is saved at {}".format(os.path.join(args.save_path, txt_file_name)))
+    fh.close()
+
---
+++ partial_spoof_detection/readme.md
@@ -0,0 +1,32 @@
+## 1. Env setup
+It is recommended that you install Python 3.8 or higher. We followed the installation setup in this project [SSL_Anti-spoofing](https://github.com/TakHemlata/SSL_Anti-spoofing), which is presented as follows:
+
+```bash
+conda create -n ps_baseline python=3.8.0 numpy=1.23.5
+conda activate ps_baseline
+pip install pip==22.3.1
+pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip install soundfile==0.11.0 librosa==0.9.1 omegaconf==2.0.6
+pip install conformer
+pip install scikit-learn pandas==1.3.5
+--------------install fairseq for XLSR--------------
+git clone https://github.com/TakHemlata/SSL_Anti-spoofing.git
+cd SSL_Anti-spoofing/fairseq-a54021305d6b3c4c5959ac9395135f63202db8f1
+pip install --editable ./
+cd ../..
+```
+
+
+## Pretrained Model
+The pretrained model XLSR can be found at [link](https://dl.fbaipublicfiles.com/fairseq/wav2vec/xlsr2_300m.pt).
+
+We have uploaded pretrained models of our experiments. You can download pretrained models from [link](https://entuedu-my.sharepoint.com/:u:/g/personal/zhlim_staff_main_ntu_edu_sg/ESZCiJIcf_1GnbRa0frYKPoBUjt4EZfUgtaoLnZF61FpNw?e=P9MJFO). 
+
+### 3. Run Inference
+Inference the pretrained model with single wav file:
+```
+python inference.py --data_path path/to/wav_file  --ckpt_path path/to/model_ckpt
+```
+Some audio samples can be found at `audio_samples/`
+## Acknowledge
+Our work is built upon the [CFPRF](https://github.com/ItzJuny/CFPRF) codebase.
